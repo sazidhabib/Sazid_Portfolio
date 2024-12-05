1a9e393a7af2e199fe94379fd88e8928d55db91b
Mahbub Sazid Habib <68610034+sazidhabib@users.noreply.github.com>
2024-12-05 21:54:16 +0600

    last update

--- project_3D_developer_portfolio-main/src/components/Skills.jsx
+++ project_3D_developer_portfolio-main/src/components/Skills.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
-import Tilt from "react-tilt";
+import Tilt from "react-parallax-tilt";
 import { skills } from "../constants/Constants";
 
 const fadeIn = (
