0583ed659d3cc6a477aaa893cf32c4b92b3f6e93
Mahbub Sazid Habib <68610034+sazidhabib@users.noreply.github.com>
2024-12-06 22:04:56 +0600

    Project navigation Update

--- project_3D_developer_portfolio-main/src/components/Hero.jsx
+++ project_3D_developer_portfolio-main/src/components/Hero.jsx
@@ -46,7 +46,7 @@ const Hero = () => {
       </div>
 
       {/* Scroll Indicator */}
-      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+      <div className="absolute xs:bottom-10 md:bottom-1 lg:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
             <motion.div
